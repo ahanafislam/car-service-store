8c32ae771341ab9d1674821cd24893e6f3242da0
ahanafislam <ahanafislam@gmail.com>
2022-04-15 06:16:12 +0600

    Create banner for site

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
+            </Navbar> 
         </>
     );
 };
