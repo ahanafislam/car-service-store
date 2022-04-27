bcb9057986b816011dcb71e3b62c82d1ea34a5e0
ahanafislam <ahanafislam@gmail.com>
2022-04-28 02:15:58 +0600

    Use axios Interceptors

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import axios from 'axios';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivate';
 
 const Order = () => {
     const [user] = useAuthState(auth);
@@ -17,11 +17,7 @@ const Order = () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
             try {
-                const {data} = await axios.get(url, {
-                    headers: {
-                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                    }
-                });
+                const {data} = await axiosPrivate.get(url);
                 setOrders(data);
             }
             catch(error) {
